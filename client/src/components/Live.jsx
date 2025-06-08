51b1da0e4dda86745d7b7e46c2dd0ca401954427
vansh <krvansh30@gmail.com>
2025-06-08 22:18:15 +0530

    feat: implementation of cursor chat and mode

--- client/src/components/Live.jsx
+++ client/src/components/Live.jsx
@@ -1,9 +1,12 @@
 import { useMyPresence, useOthers } from "@liveblocks/react";
-import React, { use } from "react";
+import React, { use, useEffect, useState } from "react";
 import LiveCursors from "./cursor/LiveCursors";
+import { CursorMode } from "@/types/type";
+import CursorChat from "./cursor/CursorChat";
 
 const Live = () => {
   const [{ cursor }, updateMyPresence] = useMyPresence();
+  const [cursorState, setCursorState] = useState({ mode: CursorMode.Hidden });
 
   const handlePointerMove = (event) => {
     event.preventDefault();
@@ -13,6 +16,8 @@ const Live = () => {
     updateMyPresence({ cursor: { x, y } });
   };
   const handlePointerLeave = () => {
+    setCursorState({ mode: CursorMode.Hidden });
+    // Clear the cursor position when the pointer leaves the area
     updateMyPresence({ cursor: null, message: null });
   };
   const handlePointerDown = (event) => {
@@ -20,6 +25,35 @@ const Live = () => {
     const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
     updateMyPresence({ cursor: { x, y } });
   };
+  useEffect(() => {
+    const onKeyUp = (e) => {
+      if (e.key === "/") {
+        setCursorState({
+          mode: CursorMode.Chat,
+          previousMessage: null,
+          message: "",
+        });
+      } else if (e.key === "Escape") {
+        updateMyPresence({ cursor: null, message: null });
+        setCursorState({ mode: CursorMode.Hidden });
+      } else if (e.key === "e") {
+        setCursorState({ mode: CursorMode.ReactionSelector });
+      }
+    };
+    const onKeyDown = (e) => {
+      if (e.key === "/") {
+        e.preventDefault();
+      }
+    };
+
+    window.addEventListener("keyup", onKeyUp);
+    window.addEventListener("keydown", onKeyDown);
+
+    return () => {
+      window.removeEventListener("keyup", onKeyUp);
+      window.removeEventListener("keydown", onKeyDown);
+    };
+  }, [updateMyPresence]);
   return (
     <div
       onPointerMove={handlePointerMove}
@@ -27,9 +61,16 @@ const Live = () => {
       onPointerDown={handlePointerDown}
       className="h-[100vh] w-full flex flex-col items-center justify-center text-center bg-primary-grey-200"
     >
-      <h1 className="text-2xl text-white">
-        <LiveCursors />
-      </h1>
+      {" "}
+      <LiveCursors />
+      {cursor && (
+        <CursorChat
+          cursor={cursor}
+          cursorState={cursorState}
+          setCursorState={setCursorState}
+          updateMyPresence={updateMyPresence}
+        />
+      )}
     </div>
   );
 };
---
+++ client/src/types/type.js
@@ -0,0 +1,6 @@
+export const CursorMode = {
+  Hidden: 0,
+  Chat: 1,
+  ReactionSelector: 2,
+  Reaction: 3,
+};
